5845c21d51dbb70eb3abffc84113e6e524493236
Alla Sariieva <alla.cleo@gmail.com>
2019-12-20 16:34:38 +0200

    finished

---
+++ build/precache-manifest.48da4db6414e7be75c8c7e2c2be5d06e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de7a571a93b6e260c2aa613a8bb6004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd483922fe4c79b5eb07",
+    "url": "/static/css/main.1957bbaf.chunk.css"
+  },
+  {
+    "revision": "f5684c8c633d144d5d24",
+    "url": "/static/js/2.b963debb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b963debb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd483922fe4c79b5eb07",
+    "url": "/static/js/main.236bb23a.chunk.js"
+  },
+  {
+    "revision": "d09cc9b20d70c9aaf5df",
+    "url": "/static/js/runtime-main.ad590475.js"
+  }
+]);
